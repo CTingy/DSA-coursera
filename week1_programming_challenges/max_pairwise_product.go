bfb2c9e9101004c601c7c5a09e86e4ce21612a37
CTingy <joker1618@livemail.tw>
2021-03-28 19:15:45 +0800

    Ty week1 (#1)

---
+++ week1_programming_challenges/max_pairwise_product.go
@@ -0,0 +1,65 @@
+// Failed case #5/17: Wrong answer
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func maxPairwiseProduct(numbers []int64) int64 {
+	maxIdx := 0
+	max := numbers[maxIdx]
+	for i := 1; i < len(numbers); i++ {
+		if numbers[i] > max {
+			maxIdx = i
+			max = numbers[i]
+		}
+	}
+	secMax := int64(0)
+	for i := 0; i < len(numbers); i++ {
+		if numbers[i] > secMax && i != maxIdx {
+			secMax = numbers[i]
+		}
+	}
+	return max * secMax
+}
+
+func maxPairwiseProductSwap(numbers []int64) int64 {
+	if len(numbers) < 2 {
+		return int64(0)
+	}
+	for i := 1; i < len(numbers); i++ {
+		if numbers[i] > numbers[0] {
+			tmp1, tmp2 := numbers[0], numbers[i]
+			numbers[i], numbers[0] = tmp1, tmp2
+		}
+	}
+	for i := 2; i < len(numbers); i++ {
+		if numbers[i] > numbers[1] {
+			tmp1, tmp2 := numbers[1], numbers[i]
+			numbers[i], numbers[1] = tmp1, tmp2
+		}
+	}
+	return numbers[0] * numbers[1]
+}
+
+func main() {
+	var numLen int
+	fmt.Scanf("%d", &numLen)
+
+	var numbers []int64
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	input := scanner.Text()
+	texts := strings.Split(input, " ")
+	for _, text := range texts {
+		number, _ := strconv.ParseInt(text, 10, 64)
+		numbers = append(numbers, number)
+	}
+	// fmt.Println(maxPairwiseProduct(numbers))
+	fmt.Println(maxPairwiseProductSwap(numbers))
+}
