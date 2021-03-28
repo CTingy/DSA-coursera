62529f03c7da229eab411e46608ce60597a6a55e
tingyun <tingyun.chan@onedegree.hk>
2021-03-28 17:20:45 +0800

    print res

--- week1_programming_challenges/max_pairwise_product.go
+++ week1_programming_challenges/max_pairwise_product.go
@@ -1,13 +1,46 @@
 package main
 
 import (
-	"strings"
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
+func maxPairwiseProduct(numbers []int) int {
+	maxIdx := 0
+	max := numbers[maxIdx]
+	for i := 1; i < len(numbers); i++ {
+		fmt.Println(i, numbers[i])
+		if numbers[i] > max {
+			maxIdx = i
+			max = numbers[i]
+		}
+	}
+	fmt.Println(maxIdx, max)
+	secMax := numbers[0]
+	for i := 1; i < len(numbers); i++ {
+		if numbers[i] > secMax && i != maxIdx {
+			secMax = numbers[i]
+		}
+	}
+	fmt.Println(secMax)
+	return max * secMax
+}
+
 func main() {
-	var inputString string
-	fmt.Scanln(&inputString)
-	s := strings.Split(inputString, " ")
-	fmt.Println(s)
+	var numLen int
+	fmt.Scanf("%d", &numLen)
+
+	var numbers []int
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	input := scanner.Text()
+	texts := strings.Split(input, " ")
+	for _, text := range texts {
+		number, _ := strconv.Atoi(text)
+		numbers = append(numbers, number)
+	}
+	fmt.Println(maxPairwiseProduct(numbers))
 }
