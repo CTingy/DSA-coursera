d39472ce1bdc1a2db0fd1180247ade749f54fdeb
tingyun <tingyun.chan@onedegree.hk>
2021-04-18 14:38:16 +0800

    fibonacci

---
+++ week2_algorithmic_warmup/fibonacci.py
@@ -0,0 +1,13 @@
+
+def fibonacci(n):
+    if n <= 1:
+        return n
+    fibonacci_list = [0, 1]
+    for i in range(2, n + 1):
+        fibonacci_list.append(fibonacci_list[i-1] + fibonacci_list[i-2])
+    return fibonacci_list[-1]
+
+
+if __name__ == '__main__':
+    input_n = int(input())
+    print(fibonacci(input_n))
