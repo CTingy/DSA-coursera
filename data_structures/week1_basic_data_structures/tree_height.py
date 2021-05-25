dcfed12f04baef05d5296f129e5f807a68a34eba
tingyun <joker1618@livemail.tw>
2021-05-25 23:26:44 +0800

    tree_height.py iterate ver

--- data_structures/week1_basic_data_structures/tree_height.py
+++ data_structures/week1_basic_data_structures/tree_height.py
@@ -1,25 +1,28 @@
 from collections import defaultdict
 
 
-def get_tree_height(sub_nodes, nodes):
-    if not sub_nodes:
-        return 1
-    max_height = 0
-    for node in sub_nodes:
-        height = get_tree_height(nodes[node], nodes)
-        if height > max_height:
-            max_height = height
-    return max_height + 1
+def get_tree_height(node_list, nodes_dict):
+    height = 1
+
+    # get_next_level_nodes
+    while node_list:
+        sub_node_list = []
+        for node in node_list:
+            sub_node_list.extend(nodes_dict[node])
+        node_list = sub_node_list
+        height += 1
+
+    return height
 
 
 if __name__ == "__main__":
     n = input()
     elements = [int(_) for _ in input().split(' ')]
-    nodes = defaultdict(list)
+    nodes_dict = defaultdict(list)
     for i in range(len(elements)):
         if elements[i] == -1:
             root = i
         else:
-            nodes[elements[i]].append(i)
+            nodes_dict[elements[i]].append(i)
 
-    print(get_tree_height(nodes[root], nodes))
+    print(get_tree_height(nodes_dict[root], nodes_dict))
