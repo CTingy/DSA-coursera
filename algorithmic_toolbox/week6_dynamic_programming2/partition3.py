0793ca3f0f5f2388703f10661c4fafde87da4f40
tingyun <joker1618@livemail.tw>
2021-05-30 21:00:29 +0800

    ref leetcode solution

--- algorithmic_toolbox/week6_dynamic_programming2/partition3.py
+++ algorithmic_toolbox/week6_dynamic_programming2/partition3.py
@@ -42,10 +42,33 @@ def partition(value, elements):
     return dp[value][len(elements)], new_elements
 
 
+def k_partition(nums, k):
+	target, m = divmod(sum(nums), k)
+	if m: return False
+	dp, n = [0]*k, len(nums)
+	nums.sort(reverse=True)
+	def dfs(i):
+		if i == n:
+			return len(set(dp)) == 1
+		for j in range(k):
+			dp[j] += nums[i]
+			if dp[j] <= target and dfs(i+1):
+				return True
+			dp[j] -= nums[i]
+			if not dp[j]: break
+		return False
+	return nums[0] <= target and dfs(0)
+
+
 # [4,4,6,2,3,8,10,2,10,7]
 # 4
 if __name__ == '__main__':
     input()
     elements = [int(x) for x in input().split(' ')]
-    elements.sort()
-    print(n_partition(3, sum(elements), elements[::-1]))
+    # elements.sort()
+    # print(n_partition(3, sum(elements), elements[::-1]))
+    can_partition = k_partition(elements, 3)
+    if can_partition:
+        print(1)
+    else:
+        print(0)
