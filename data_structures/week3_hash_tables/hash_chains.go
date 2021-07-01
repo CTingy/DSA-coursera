13d09f2490b0c2980c3a05e412de061678e2eb86
tingyun <joker1618@livemail.tw>
2021-07-01 20:24:58 +0800

    hash_chains.go

---
+++ data_structures/week3_hash_tables/hash_chains.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+)
+
+const (
+	x = 263
+	p = 1000000007
+)
+
+type query struct {
+	action  string
+	content string
+}
+
+type hashChain struct {
+	size   int
+	chains [][]string
+}
+
+func RemoveIndex(s []string, index int) []string {
+	return append(s[:index], s[index+1:]...)
+}
+
+func H(s string, m int) int {
+	res, power := 0, 0
+	for idx, char := range s {
+		power = int(math.Pow(x, float64(idx)))
+		res = (res + int(char)*power) % p
+	}
+	return res % m
+}
+
+func (hc hashChain) add(key int, value string) {
+	hc.chains[key] = append([]string{value}, hc.chains[key]...)
+}
+
+func (hc hashChain) check(key int) {
+	values := hc.chains[key]
+	if len(values) > 0 {
+		var s string
+		for _, value := range values {
+			s += fmt.Sprintf("%s ", value)
+		}
+		fmt.Println(s)
+	}
+}
+
+func (hc hashChain) find(content string) {
+	for _, chain := range hc.chains {
+		for _, value := range chain {
+			if value == content {
+				fmt.Println("yes")
+				return
+			}
+		}
+	}
+	fmt.Println("no")
+}
+
+func (hc hashChain) del(content string) {
+	for key, chain := range hc.chains {
+		for idx, value := range chain {
+			if value == content {
+				hc.chains[key] = RemoveIndex(chain, idx)
+			}
+		}
+	}
+}
+
+func execQuery(hc hashChain, q query) {
+	if q.action == "add" {
+		hc.add(H(q.content, hc.size), q.content)
+	} else if q.action == "check" {
+		key, _ := strconv.Atoi(q.content)
+		hc.check(key)
+	} else if q.action == "find" {
+		hc.find(q.content)
+	} else if q.action == "del" {
+		hc.del(q.content)
+	}
+
+}
+
+func main() {
+	var m, num int
+	fmt.Scanln(&m)
+	fmt.Scanln(&num)
+
+	queries := make([]query, num)
+	for i := 0; i < num; i++ {
+		var s1, s2 string
+		fmt.Scanf("%s %s", &s1, &s2)
+		queries[i] = query{action: s1, content: s2}
+	}
+
+	hc := hashChain{size: m, chains: make([][]string, m)}
+	for _, q := range queries {
+		execQuery(hc, q)
+	}
+	// fmt.Println(hc.chains)
+}
